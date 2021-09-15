b98049825a0437f955d147bc71b06051f4099cb9
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2021-09-15 18:00:52 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Create a web app with a button loded. Show a text loading... on a web app. However, hide it if I click on the button loaded.
---
+++ script.js
@@ -0,0 +1,6 @@
+const text= document.querySelector(".text");
+const button=document.querySelector(".loaded");
+
+button.addEventListener("click",()=>{
+  text.style.display="none";
+})
